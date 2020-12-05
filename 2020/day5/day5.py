e56d9136d35aedf8404354b395d4aa8d9dd730a7
Adam McKerlie <adam.mckerlie@sentry.io>
2020-12-05 07:19:51 -0500

    Day 5, part 2

--- 2020/day5/day5.py
+++ 2020/day5/day5.py
@@ -33,6 +33,24 @@ def part_1():
 
     return max_seat
 
+def part_2():
+    seat_ids = []
+    for seat in SEATS:
+        seat_row = seat_search(seat[:7], 0, 0, 127)
+        seat_col = seat_search(seat[7:10], 0, 0, 7)
+        seat_id = seat_row * 8 + seat_col
+        seat_ids.append(seat_id)
+
+    # Sum up all of the seat IDs from the lowest one to the highest one.
+    expected_total = sum(range(min(seat_ids), max(seat_ids)+1))
+    
+    # Sum up the IDs from the seats we've generated
+    total = sum([seat for seat in seat_ids])
+
+    return expected_total - total
+
+
 
 process_seats()
 print("Answer for part 1: {}".format(part_1()))
+print("Answer for part 2: {}".format(part_2()))
