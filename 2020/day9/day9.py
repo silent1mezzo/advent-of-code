851392748d901ae78e340503199fe29438b88872
Adam McKerlie <adam.mckerlie@sentry.io>
2020-12-09 07:06:56 -0500

    Day 9, part 2

--- 2020/day9/day9.py
+++ 2020/day9/day9.py
@@ -1,4 +1,4 @@
-from itertools import combinations
+from itertools import accumulate, combinations
 
 PREAMBLE = 25
 data = [None] * PREAMBLE
@@ -20,5 +20,21 @@ def part_1():
             data[count % PREAMBLE] = int(line.strip())
             count += 1
 
+def part_2():
+    INVALID_NUM = part_1()
 
-print("Answer for part 1: {}".format(part_1()))
+    with open('input.txt') as reader:
+        count = 0
+        for line in reader.readlines():
+            if count >= PREAMBLE:
+                VALID = False
+                for idx, d in enumerate(accumulate(data)):
+                    if d == INVALID_NUM:
+                        return min(data[:idx]) + max(data[:idx])
+
+            data[count % PREAMBLE] = int(line.strip())
+            count += 1
+
+
+#rint("Answer for part 1: {}".format(part_1()))
+print("Answer for part 2: {}".format(part_2()))
