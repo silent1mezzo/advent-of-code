14bec3f748382ca7623c9e6fa55fffa444b73138
Adam McKerlie <adammckerlie@gmail.com>
2023-12-01 09:16:24 -0500

    Day 1 of 2022

--- 2022/day02/day02.py
+++ 2022/day02/day02.py
@@ -1,11 +1,38 @@
-data = []
+MOVES = []
+
+ROCK = 'A'
+PAPER = 'B'
+SCISSORS = 'C'
+
+SCORES = {
+    PAPER: 2,
+    ROCK: 1,
+    SCISSORS: 3,
+    'LOSE': 0,
+    'DRAW': 3,
+    'WIN': 6
+}
+
+def calculate_outcome(their_move, your_move)
+    if their_move == your_move:
+        return 'DRAW'
+    elif (their_move == ROCK && your_move=PAPER) || (their_move == PAPER && your_move=SCISSORS) || (their_move == SCISSORS && your_move=ROCK)  
+        return 'WIN'
+    return 'LOSE'
 
 def read_input():
     with open('input.txt') as reader:
         for line in reader.readlines():
-            data.append(int(line))
+            moves = line.split(' ')
+            your_move = ROCK if moves[1] == 'X' elif PAPER if moves[1] == 'Y' else SCISSORS,
+            MOVES = {
+                'theirs': moves[0],
+                'yours': your_move,
+                'outcome': calculate_outcome(moves[0], your_move)
+            }
 
 def part_1():
+    print(MOVES)
     pass
 
 def part_2():
