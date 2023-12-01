d681492f0994158265996a46f30ac7fb115fcbb2
Adam McKerlie <adammckerlie@gmail.com>
2023-12-01 08:56:33 -0500

    Add all days

---
+++ 2023/day25/day25.py
@@ -0,0 +1,17 @@
+data = []
+
+def read_input():
+    with open('input.txt') as reader:
+        for line in reader.readlines():
+            data.append(int(line))
+
+def part_1():
+    pass
+
+def part_2():
+    pass
+
+
+read_input()
+print(f"Answer for part 1: {part_1()}")
+print(f"Answer for part 2: {part_2()}")
