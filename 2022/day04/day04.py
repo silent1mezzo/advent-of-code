c5f96b9d9bf2de7bf35f064a83cf80899f800eaa
Adam McKerlie <adammckerlie@gmail.com>
2023-12-01 11:18:13 -0500

    AOC 2022 day4

--- 2022/day04/day04.py
+++ 2022/day04/day04.py
@@ -3,13 +3,34 @@ data = []
 def read_input():
     with open('input.txt') as reader:
         for line in reader.readlines():
-            data.append(int(line))
+            data.append(line.replace('\n','').split(','))
 
 def part_1():
-    pass
+    total = 0
+    for line in data:
+        first_range = line[0].split('-')
+        second_range = line[1].split('-')
+        first = set(range(int(first_range[0]), int(first_range[1])+1))
+        second = set(range(int(second_range[0]), int(second_range[1])+1))
+
+        if first.issubset(second) or second.issubset(first):
+            total += 1
+
+    return total
 
 def part_2():
-    pass
+    total = 0
+    for line in data:
+        first_range = line[0].split('-')
+        second_range = line[1].split('-')
+        first = set(range(int(first_range[0]), int(first_range[1])+1))
+        second = set(range(int(second_range[0]), int(second_range[1])+1))
+
+        if not set(first).isdisjoint(second):
+            total += 1
+
+    return total
+
 
 
 read_input()
