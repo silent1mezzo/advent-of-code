82e88421dc61ab5b71e4e20ba2a534a12e553ab4
Adam McKerlie <adammckerlie@gmail.com>
2023-12-11 08:09:20 -0500

    AOC2023 Day 5 part 1

--- 2023/day05/day05.py
+++ 2023/day05/day05.py
@@ -1,12 +1,85 @@
-data = []
+import re
+
+SEEDS = []
+
+'''
+e.g.
+seed-to-soil map:
+50 98 2
+52 50 48
+
+# Map [
+    {'diff': -48, 'source-low': 98, 'source-high': 99, 'destination-low': 50, 'destination-high': 51},
+    {'diff': 2, 'source-low': 50, 'source-high': 97, 'destination-low': 52, 'destination-high': 99},
+]
+'''
+SEED_TO_SOIL = []
+SOIL_TO_FERT = []
+FERT_TO_WATER = []
+WATER_TO_LIGHT = []
+LIGHT_TO_TEMP = []
+TEMP_TO_HUMIDITY = []
+HUMIDITY_TO_LOCATION = []
+
 
 def read_input():
     with open('input.txt') as reader:
         for line in reader.readlines():
-            data.append(int(line))
+            if 'seeds:' in line:
+                SEEDS.extend(re.findall(r'\d+', line))
+            elif 'seed-to-soil map:' in line:
+                CUR_ARRAY = SEED_TO_SOIL
+            elif 'soil-to-fertilizer map:' in line:
+                CUR_ARRAY = SOIL_TO_FERT
+            elif 'fertilizer-to-water map:' in line:
+                CUR_ARRAY = FERT_TO_WATER
+            elif 'water-to-light map:' in line:
+                CUR_ARRAY = WATER_TO_LIGHT
+            elif 'light-to-temperature map:' in line:
+                CUR_ARRAY = LIGHT_TO_TEMP
+            elif 'temperature-to-humidity map:' in line:
+                CUR_ARRAY = TEMP_TO_HUMIDITY
+            elif 'humidity-to-location map:' in line:
+                CUR_ARRAY = HUMIDITY_TO_LOCATION
+            elif line[0].isdigit():
+                data = line.replace('\n', '').split(' ')
+                source = int(data[1])
+                destination = int(data[0])
+                length = int(data[2])
+                CUR_ARRAY.append(
+                    {
+                        'diff': destination - source,
+                        'source-low': source,
+                        'source-high': source + length - 1,
+                        'destination-low': destination,
+                        'destination-high': destination + length - 1,
+                    },
+                )
+
+def get_map_value(source, MAP):
+    source = int(source)
+    for line in MAP:
+        if source >= line['source-low'] and source <= line['source-high']:
+            return source + line['diff']
+
+    return source
+
+
 
 def part_1():
-    pass
+    min_location = -1
+    for seed in SEEDS:
+        soil = get_map_value(seed, SEED_TO_SOIL)
+        fert = get_map_value(soil, SOIL_TO_FERT)
+        water = get_map_value(fert, FERT_TO_WATER)
+        light = get_map_value(water, WATER_TO_LIGHT)
+        temp = get_map_value(light, LIGHT_TO_TEMP)
+        humidity = get_map_value(temp, TEMP_TO_HUMIDITY)
+        location = get_map_value(humidity, HUMIDITY_TO_LOCATION)
+        if min_location == -1 or location <= min_location:
+            min_location = location
+
+    return min_location
 
 def part_2():
     pass
