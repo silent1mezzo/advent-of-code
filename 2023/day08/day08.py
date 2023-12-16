145147c57108471b1ca0a4794c98965ed0bc476e
Adam McKerlie <adammckerlie@gmail.com>
2023-12-16 11:23:24 -0500

    Part 2

--- 2023/day08/day08.py
+++ 2023/day08/day08.py
@@ -1,8 +1,9 @@
 data = {}
+current_indexes = []
 instructions = ''
 
 def read_input():
-    global instructions
+    global instructions, current_indexes
     with open('input.txt') as reader:
         line = reader.readline().strip()
         instructions = line.replace('\n', '')
@@ -11,11 +12,12 @@ def read_input():
             if line != '':
                 parts = line.split('=')
                 other = parts[1].replace('(', '').replace(')', '').split(',')
+                if parts[0].strip().endswith('A'):
+                    current_indexes.append(parts[0].strip())
                 data[parts[0].strip()] = {
                     'left': other[0].strip(),
                     'right': other[1].strip(),
                 }
-    print(instructions, data)
 
 def part_1():
     step = 0
@@ -35,8 +37,25 @@ def part_1():
 
 
 def part_2():
-    pass
+    step = 0
+    global current_indexes
 
+    while True:
+        for instruction in instructions:
+            step += 1
+            new_indexes = []
+            if instruction == 'L':
+                direction = 'left'
+            else:
+                direction = 'right'
+
+            for index in current_indexes:
+                    new_indexes.append(data[index][direction])
+            
+            if all(x.endswith('Z') for x in new_indexes):
+                return step
+
+            current_indexes = new_indexes
 
 read_input()
 print(f"Answer for part 1: {part_1()}")
