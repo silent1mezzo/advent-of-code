b14f33d3ebc07cb2b54ec267f944e09e11f09aa3
Adam McKerlie <adammckerlie@gmail.com>
2023-12-16 09:16:01 -0500

    AOC 2023 Day 8

--- 2023/day08/day08.py
+++ 2023/day08/day08.py
@@ -1,12 +1,38 @@
-data = []
+data = {}
+instructions = ''
 
 def read_input():
+    global instructions
     with open('input.txt') as reader:
+        line = reader.readline().strip()
+        instructions = line.replace('\n', '')
         for line in reader.readlines():
-            data.append(int(line))
+            line = line.replace('\n', '')
+            if line != '':
+                parts = line.split('=')
+                other = parts[1].replace('(', '').replace(')', '').split(',')
+                data[parts[0].strip()] = {
+                    'left': other[0].strip(),
+                    'right': other[1].strip(),
+                }
+    print(instructions, data)
 
 def part_1():
-    pass
+    step = 0
+    current_index = 'AAA'
+
+    while True:
+        for instruction in instructions:
+            step +=1
+            
+            if instruction == 'L':
+                current_index = data[current_index]['left']
+            else:
+                current_index = data[current_index]['right']
+        
+            if current_index == 'ZZZ':
+                return step
+
 
 def part_2():
     pass
